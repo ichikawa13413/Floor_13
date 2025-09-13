4dad0c41a29f187fbb6d473468f946e1f4938db1
ichikawa13413 <ichikawa13413@gmail.com>
2025-09-13 16:24:59 +0900

    #21実装完了しました

--- Assets/ViweScripts/GameUIManager.cs
+++ Assets/ViweScripts/GameUIManager.cs
@@ -1,6 +1,8 @@
 using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.UI;
 using VContainer;
 
@@ -9,6 +11,7 @@ public class GameUIManager : MonoBehaviour
     private Transform _transform;
     private Canvas _canvas;
     private SceneLoadManager _sceneLoadManager;
+
     //--ゲームオーバーテキスト--
     [SerializeField] private TextMeshProUGUI gameOverText;
     [SerializeField] private Vector3 gameOverTextPos;
@@ -20,7 +23,12 @@ public class GameUIManager : MonoBehaviour
     [SerializeField] private Vector3 QuitButtonPos;
 
     //--ゲーム終了時にでる警告画面--
-    [SerializeField] private Image CautionPrefab;
+    [SerializeField] private Image cautionPrefab;
+    [SerializeField] private Button yesButtonPrefab;
+    [SerializeField] private Button noButtonPrefab;
+    [SerializeField] private Vector3 yesButtonPos;
+    [SerializeField] private Vector3 noButtonPos;
+    private Image cautionImage;
 
     [Inject]
     public void Construct(Canvas canvas, SceneLoadManager sceneLoadManager)
@@ -43,16 +51,29 @@ public class GameUIManager : MonoBehaviour
     //コンテニューボタンとクイットボタンを指定座標に生成
     public void CreateContinueButton()
     {
-        var continueButton = Instantiate(ContinueButtonPrefab, _canvas.transform);
-        var quitButton = Instantiate(QuitButtonPredab, _canvas.transform);
+        var continueButtonHighlight = Instantiate(ContinueButtonPrefab, _canvas.transform);
+        var quitButtonHighlight = Instantiate(QuitButtonPredab, _canvas.transform);
 
-        Button button1 = continueButton.gameObject.GetComponentInChildren<Button>(false);
-        Button button2 = quitButton.gameObject.GetComponentInChildren<Button>(false);
+        Button continueButton = continueButtonHighlight.gameObject.GetComponentInChildren<Button>(false);
+        Button quitButton = quitButtonHighlight.gameObject.GetComponentInChildren<Button>(false);
 
-        button1.onClick.AddListener(_sceneLoadManager.ContinueFunction);
-        button2.onClick.AddListener(_sceneLoadManager.QuitFunction);
+        continueButton.onClick.AddListener(_sceneLoadManager.ContinueFunction);
+        quitButton.onClick.AddListener(CreateCaution);
 
-        continueButton.GetComponent<RectTransform>().anchoredPosition = ContinueButtonPos;
-        quitButton.GetComponent<RectTransform>().anchoredPosition = QuitButtonPos;
+        continueButtonHighlight.GetComponent<RectTransform>().anchoredPosition = ContinueButtonPos;
+        quitButtonHighlight.GetComponent<RectTransform>().anchoredPosition = QuitButtonPos;
+    }
+
+    private void CreateCaution()
+    {
+        cautionImage = Instantiate(cautionPrefab, _canvas.transform);
+        Button yesButton = Instantiate(yesButtonPrefab, cautionImage.transform);
+        Button noButton = Instantiate(noButtonPrefab, cautionImage.transform);
+
+        yesButton.GetComponent<RectTransform>().anchoredPosition = yesButtonPos;
+        noButton.GetComponent<RectTransform>().anchoredPosition= noButtonPos;
+
+        yesButton.onClick.AddListener(_sceneLoadManager.QuitFunction);
+        noButton.onClick.AddListener(() => Destroy(cautionImage.gameObject));
     }
 }
